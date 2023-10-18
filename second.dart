c229847b2f44336e59e049f991b38c842d3cfc74
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-10-18 21:20:23 +0100

    added two objects and the main function

--- second.dart
+++ second.dart
@@ -1,14 +1,30 @@
 class Doc{
   String id;
   String? name;
+  String ?content;
 
-  Doc(this.id){
+  Doc(this.id, {this.name, this.content}){
     
   }
 }
 
+
+class Teacher{
+
+  String id;
+  String name;
+  double salary;
+
+  Teacher({required this.id, required this.name, required this.salary}){
+
+  }
+}
 void main(List<String> args) {
   Doc document = Doc("id");
+  final doc = Doc('erttt',name: 'News',content: "Information about news");
+  var teacher = Teacher(id: "yfbfhbbnf", name: "Abel Thomas", salary: 344647.90);
 
   print(document.toString());
+   print(doc.toString());
+    print(teacher.toString());
 }
